0040d6f31d52b70953a025311dd8dbb1de77a384
vimalaad <vimalaad@gmail.com>
2014-06-11 00:18:03 +0430

    neo2.1.1 tested

---
+++ DataImport/sampleRun.bat
@@ -0,0 +1,6 @@
+neo4jshell.bat -path ..\data\graph.db -file C:\Users\AliReza\Downloads\cypherbuilderfornaft\DataImport\CreateBaseNodes.txt
+neo4jshell.bat -path ..\data\graph.db -file C:\Users\AliReza\Downloads\cypherbuilderfornaft\DataImport\outPut.txt
+neo4jshell.bat -path ..\data\graph.db -file C:\Users\AliReza\Downloads\cypherbuilderfornaft\DataImport\outPutCNT.txt
+neo4jshell.bat -path ..\data\graph.db -file C:\Users\AliReza\Downloads\cypherbuilderfornaft\DataImport\CreateGorooh.txt
+neo4jshell.bat -path ..\data\graph.db -file C:\Users\AliReza\Downloads\cypherbuilderfornaft\DataImport\CreateRelations.txt
+neo4jshell.bat -path ..\data\graph.db -file C:\Users\AliReza\Downloads\cypherbuilderfornaft\DataImport\CreateRelationsMohtava.txt
